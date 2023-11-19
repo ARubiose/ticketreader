7923d007244ef3281b0aa6b4d98e4ce7663ace00
Arubiose <alvaro.rubio.segovia@gmail.com>
2023-11-19 20:31:22 +0100

    Update log file path

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 """Main script for ticketreader"""
 from ticketreader import api
 from ticketreader import config
+from ticketreader import mercadona
 
 
 def main() -> None:
     """Main function"""
-    api.parse_mercadona_directory(
-        ticket_directory=config.DATA_DIR / 'Madrid',
-        destination=config.DATA_DIR / 'tickets-madrid.xlsx'
-    )
+    # api.parse_mercadona_directory(
+    #     ticket_directory=config.DATA_DIR /,
+    #     destination=config.DATA_DIR / 'tickets-template.xlsx'
+    # )
+    ticket = mercadona.parse_mercadona_ticket_tabula(
+        file_path=config.DATA_DIR / 'Madrid' / 'Saved' / 'my-ticket.pdf')
+    print(ticket)
 
 
 if __name__ == "__main__":
